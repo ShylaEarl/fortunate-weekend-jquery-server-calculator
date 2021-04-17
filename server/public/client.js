917a183d68f8534bd548e84361bc097341470eb6
Shyla Earl <shyla.earl@gmail.com>
2021-04-17 10:45:43 -0500

    calculation function on server functional, get request on client functional, appending calculation total to browser

--- server/public/client.js
+++ server/public/client.js
@@ -18,11 +18,12 @@ function onReady(){
 }//end onReady
 
 function clearInputFields(){
+    //sets client/browser number input feilds to empty strings, emptying them out
     $('#first-number').val('');
     $('#second-number').val('');
 }
 
-//function to capture value of user/DOM selected operator
+//function to capture value of user/browser selected operator
 function operatorValue(){
     //assign a variable to capture operator value
     mathOperator = $(this).data('value');
@@ -33,13 +34,13 @@ function operatorValue(){
 //send input from user/DOM to server
 function postInputs(){
     console.log('in POST, clicked');
-    //get input from user/DOM, bundle it up, and...
+    //get input from user/browser, bundle it up, and...
     let mathSet = {
         firstNumber: Number($('#first-number').val()),
         secondNumber: Number($('#second-number').val()),
         mathOperator: mathOperator
     }
-    //send data to server via POST request
+    //send data to server via POST request at XXXX url with this data
     $.ajax({
         method: 'POST',
         url: '/submitInputs',
@@ -47,7 +48,8 @@ function postInputs(){
     })
         .then(function(response){
             console.log('response', response);
-            //call function to get calculation total
+            //call to get calculation total
+            getCalculation();
         })
         .catch(function(error){
             console.log('error POST to server', error);
@@ -56,5 +58,28 @@ function postInputs(){
 }//end postInputs function
 
 //TODO Write ajax 'GET' response from server
+function getCalculation(){
+    //get data from server via GET request at XXXX url... 
+    $.ajax({
+        method: 'GET',
+        url: '/submitInputs'
+    })
+        //returning this data...
+        .then(function(response){
+            console.log('response', response);
+            //and appending current total to the browser
+            $('#current-total').empty();
+            //prevents error on page load since a response does not yet exist
+            if(response.length > 0){
+                $('#current-total').append(response[response.length-1].total)
+            }
+            //call to render/append response/array/history to browser 
+            //renderHistory(response);
+        })
+        .catch(function(error){
+            console.log('Error from server', error);
+            alert('Cannot get math. Try again later.')
+        });
+}// end getCalculation 
 
 //TODO render
--- server/server.js
+++ server/server.js
@@ -14,23 +14,48 @@ app.use(bodyParser.urlencoded({extended: true}));
 //array to hold history of mathObjects
 let calculationHistory = [];
 
+//server side of ajax POST request
+app.post('/submitInputs', (req, res) => {
+    console.log('new req.body', req.body);
+    //variable to hold request object from client/browser
+    let mathObject = req.body;
+    console.log('the new math object is,', mathObject);
+    //assinging new property to request object/mathObject to hold calculation total 
+    //by calling calculation function with current mathObject
+    mathObject.total = calculate(mathObject);
+    //save new inputs object in array 
+    calculationHistory.push(mathObject);
+    res.sendStatus(201); //201 status means 'I created, added a thing...'
+})
+
+//function to calculate inputs from client/mathObject
+function calculate(mathObject){
+    //variables to hold input numbers from user/client
+    let firstNumber = mathObject.firstNumber;
+    let secondNumber = mathObject.secondNumber;
+    //switch case uses current operator to do calculation
+    switch (mathObject.mathOperator){
+        case '+':
+            total = firstNumber + secondNumber;
+            return total;
+        case '-':
+            total = firstNumber - secondNumber;
+            return total;
+        case '*':
+            total = firstNumber * secondNumber;
+            return total;
+        case '/':
+            total = firstNumber / secondNumber;
+            return total;
+    }//end switch case
+}//end calculate function
+
 //server side of ajax GET request...
 app.get('/submitInputs', (req, res) => {
     console.log('Request array...', calculationHistory);
     //sending the data back to the browser/client
     res.send(calculationHistory);
 })
-
-//server side of ajax POST request
-app.post('/submitInputs', (req, res) => {
-    let mathObject = req.body;
-    console.log('Your new calculation is', mathObject);
-    //save new inputs object in array 
-    calculationHistory.push(mathObject);
-    res.sendStatus(201); //201 status means 'I created, added a thing...'
-})
-
-//TODO add function to do calculation with inputs from client/mathObject
 
 // Start up our server
 const PORT = 5000;
